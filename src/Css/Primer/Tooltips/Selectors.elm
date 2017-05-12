6a3e4842e4fda5b90089f88ecf8ace811b355459
YetAnotherMinion <yam@thinkalexandria.com>
2017-05-12 12:38:51 -0400

    tweaked module namespace, added short documentation

--- src/Css/Primer/Tooltips/Selectors.elm
+++ src/Css/Primer/Tooltips/Selectors.elm
@@ -1,25 +1,32 @@
-module Primer.Tooltips.Selectors exposing (CssClasses(..), CssIds(..), class, classList)
+module Css.Primer.Tooltips.Selectors exposing (CssClasses(..), class, classList)
 
 {-| This elm module provides strongly typed selectors for the elm port of
 GitHub's primer-css framework
+
+@docs CssClasses, class, classList
 -}
 
 import Html.Attributes
 import Html
 
 
+{-| Helper lets you keep type checking of selectors all the way to call site.
+-}
 class : CssClasses -> Html.Attribute msg
-class name =
-    Html.Attributes.class <| toString name
+class =
+    toString >> Html.Attributes.class
 
 
+{-| Helper lets you keep type checking of selectors all the way to call site.
+-}
 classList : List (CssClasses, Bool) -> Html.Attribute msg
 classList names =
     names
-        |> List.map ((c,b) -> (toString c, b))
+        |> List.map (\(c,b) -> (toString c, b))
         |> Html.Attributes.classList
 
-
+{-| The 12 CSS classes exposed by the SASS implementation.
+-}
 type CssClasses
     = ToolTipped
     | ToolTippedMultiline
